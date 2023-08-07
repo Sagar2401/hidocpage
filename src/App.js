db715e0e4cc266607b27662dd05fee0579b60996
Sagar2401 <prajaptisagar81410@gmail.com>
2023-08-07 22:36:13 +0530

    Design update

--- src/App.js
+++ src/App.js
@@ -14,6 +14,52 @@ function App() {
     "Conferences",
   ];
 
+  const btnData = [
+    "Anesthesia",
+
+    "Cardiology",
+
+    "Critical Care",
+    "Dentistry",
+
+    "Dermatology",
+
+    "Diabetology",
+  ];
+  const otherData = [
+    {
+      icon: 1,
+      name: "Drug Database",
+    },
+    {
+      icon: 1,
+      name: "Webinars",
+    },
+    {
+      icon: 1,
+      name: "Drug Interaction",
+    },
+    {
+      icon: 1,
+      name: "Madical calculator",
+    },
+    {
+      icon: 1,
+      name: "Articles",
+    },
+    {
+      icon: 1,
+      name: "Quizzes",
+    },
+    {
+      icon: 1,
+      name: "Diseases Database",
+    },
+    {
+      icon: 1,
+      name: "Survays",
+    },
+  ];
   return (
     <div className="App">
       <header className="App-header">
@@ -62,6 +108,98 @@ function App() {
           </svg>
         </div>
       </div>
+      <div className="fourth-container">
+        <p>
+          Dailymed Drug Database The DailyMed database contains 143072 labeling
+          submitted to the Food and Drug Administration (FDA) by companies.
+          DailyMed does not contain a complete listing of labeling for
+          FDA-regulated products (eg, labeling that is not submitted to the
+          FDA).
+        </p>
+        <div className="btn-wrap">
+          {btnData.map((data) => {
+            return <button className="btn">{data}</button>;
+          })}
+        </div>
+      </div>
+      <div className="table-wrap">
+        <div className="box">
+          <div className="box-header">Drug Data</div>
+          <div className="box-conent">
+            <p>1000</p>
+            <p>1000</p>
+            <p>1000</p>
+          </div>
+        </div>
+        <div className="box">
+          <div className="box-header">Drug Details</div>
+          <div className="box-conent">
+            <p>1000</p>
+            <p>1000</p>
+            <p>1000</p>
+          </div>
+        </div>
+      </div>
+
+      <footer>
+        <div className="ft-wrap">
+          <div className="first-ftr">
+            <img
+              className="logo"
+              src="https://hidoc.co/assets/img/logo/logo3.png"
+              alt="no img"
+            />
+            <p>Fastest Growing Medical Platform for Doctors</p>
+
+            <ul>
+              <li className="li">
+                <i class="fa fa-facebook"></i>
+              </li>
+              <li className="li">
+                <i class="fa fa-instagram"></i>
+              </li>
+              <li className="li">
+                <i class="fa fa-youtube-play"></i>
+              </li>
+              <li className="li">
+                <i class="fa fa-linkedin"></i>
+              </li>
+            </ul>
+          </div>
+          <div className="second-ftr">
+            <h4>REACH US</h4>
+            <p>Please contact below details for any other information.</p>
+
+            <p className="detalis">
+              <span>Email: </span>info@hidoc.com
+            </p>
+            <p className="detalis">
+              <span>Address: </span>
+              Hidoc Dr.Inc, 2055 Limestone Rd, STE 200-C, Wilmington, DE, 19808,
+              United States
+            </p>
+          </div>
+          <div className="third-ftr">
+            <h4>HIDOC DR. FEATURES</h4>
+            <div className="other-content">
+              {otherData.map((data) => {
+                return (
+                  <div className="other-box">
+                    {data.icon}
+                    <p>{data.name}</p>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+        <div className="last-ft">
+          <p className="copyright">
+            © Copyright <strong>2022 Infedis Infotech LLP.</strong>
+          </p>
+          <p className="copyright">Terms & Condition | Privecy Policy</p>
+        </div>
+      </footer>
     </div>
   );
 }
